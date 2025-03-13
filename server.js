1b531d8653c0e9334e26a6a31b9397ac3b441ca7
PRADHANJI07 <bhabeshchandrapradhan@gcekbpatna.ac.in>
2025-03-13 13:03:16 +0530

    updated the server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userRoutes = require('./routes/userRoutes');
 const customerRoutes = require('./routes/customerRoutes');
@@ -34,3 +36,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
